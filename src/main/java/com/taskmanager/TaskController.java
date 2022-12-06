0e472a615e8c3452ac3224c09caa7d7eb6c5243d
nanderson7 <91236448+nanderson7@users.noreply.github.com>
2022-12-05 19:35:30 -0700

    Added filtering for all three fields

--- src/main/java/com/taskmanager/TaskController.java
+++ src/main/java/com/taskmanager/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -38,7 +39,6 @@ public class TaskController {
 	public String viewAllTasks(Model model) {
 		List<Task> tasks = taskRepository.findAll();
 		model.addAttribute("tasks", tasks);
-		tasks.forEach(System.out::println);
 		return "allTasks";
 	}
 	
@@ -55,5 +55,27 @@ public class TaskController {
 		taskRepository.deleteById(id);
 		redirectAttributes.addFlashAttribute("deleteSuccessful", "Task deleted successfully");
 		return "redirect:/tasks/view-all";
+	}
+	
+	@GetMapping("/filter-by-description")
+	public String filterByDescription(Model model, @RequestParam("description") String description) {
+		List<Task> filteredTasks = taskRepository.findAllByDescription(description);
+		model.addAttribute("tasks", filteredTasks);
+		return "allTasks";
+	}
+	
+	@GetMapping("/filter-by-due-date")
+	public String filterByDueDate(Model model, @RequestParam("due-date") String dueDate) {
+		List<Task> filteredTasks = taskRepository.findAllByDueDate(dueDate);
+		model.addAttribute("tasks", filteredTasks);
+		return "allTasks";
+	}
+	
+	@GetMapping("/filter-by-completion-status")
+	public String filterByCompletionStatus(Model model, @RequestParam("completion-status") String completionStatus) {
+		boolean isComplete = completionStatus.equalsIgnoreCase("yes") ? true : false;
+		List<Task> filteredTasks = taskRepository.findAllByIsComplete(isComplete);
+		model.addAttribute("tasks", filteredTasks);
+		return "allTasks";
 	}
 }
--- src/main/java/com/taskmanager/TaskRepository.java
+++ src/main/java/com/taskmanager/TaskRepository.java
@@ -1,5 +1,7 @@
 package com.taskmanager;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +13,12 @@ public interface TaskRepository extends JpaRepository<Task, Integer>{
 	@Modifying
 	@Query("UPDATE Task t SET t.isComplete = :isComplete WHERE id = :id")
 	void updateIsComplete(boolean isComplete, int id);
+	
+	@Query("SELECT t FROM Task t WHERE lower(t.description) LIKE lower(concat('%', :description, '%'))")
+	List<Task> findAllByDescription(String description);
+	
+	@Query("SELECT t FROM Task t WHERE lower(t.dueDate) LIKE lower(concat('%', :dueDate, '%'))")
+	List<Task> findAllByDueDate(String dueDate);
+	
+	List<Task> findAllByIsComplete(boolean isComplete);
 }
